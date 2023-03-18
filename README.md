077daa1114ee0eab5afecdae097197d8df73683f
wuyong <wu4yong@163.com>
2023-03-19 01:12:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-github-search
+基于vue开发的一个简单的GitHub搜索的小案例
+
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <search></search>
+    <list></list>
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+import Search from './components/Search.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Search,
+    List
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
